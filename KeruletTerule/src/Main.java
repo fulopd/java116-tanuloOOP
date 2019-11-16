bb39f0f13385f9b6d90088ad634b275d0fa91d96
Fülöp Dániel <fulopdaniel@gmail.com>
2019-11-16 11:03:05 +0100

    DecimalFormattel kiegészítve

--- KeruletTerule/src/Main.java
+++ KeruletTerule/src/Main.java
@@ -1,8 +1,10 @@
+import java.text.DecimalFormat;
+
 public class Main {
 
 	public static void main(String[] args) {
-		
-		System.out.println("Kör kerület / terület: " + Sikidomok.korKerulet(3) +" / "+ Sikidomok.korTerulet(3));
+		DecimalFormat df = new DecimalFormat("0.000");
+		System.out.println("Kör kerület / terület: " + df.format(Sikidomok.korKerulet(3)) +" / "+ df.format(Sikidomok.korTerulet(3)));
 		System.out.println("Négyzet kerület / terület: " + Sikidomok.negyzetKerulet(3) +" / "+ Sikidomok.negyzetTerulet(3));
 		System.out.println("Téglalap kerület / terület: " + Sikidomok.teglalapKerulet(2, 2) +" / "+ Sikidomok.teglalapTerulet(2, 2));
 		System.out.println("Háromszög kerület / terület: " + Sikidomok.haromszogKerulet(2, 2, 2) +" / "+ Sikidomok.haromszogTerulet(3, 3, 3));
